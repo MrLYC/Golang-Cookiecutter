6290250f4e44d5508712af52a10aa87b53fe62bb
MrLYC <imyikong@gmail.com>
2018-07-15 00:14:14 +0800

    add handlers

---
+++ {{cookiecutter.app_name}}/handlers.go
@@ -0,0 +1,31 @@
+import (
+	"math/rand"
+	"time"
+	
+	"{{cookiecutter.app_path}}/logging"
+)
+
+func initRandomSeed() bool {
+	rand.Seed(time.Now().Unix())
+	return true
+}
+
+func initConfiguration() bool {
+	var err error
+	err = config.Configuration.Read()
+	if err != nil {
+	    fmt.Fprintln(os.Stderr, err.Error())
+	}
+
+	err = config.Configuration.Validate()
+	if err != nil {
+	    fmt.Fprintln(os.Stderr, err.Error())
+	    return false
+	}
+	return true
+}
+
+func initLogging() bool {
+	logging.Init()
+	return true
+}
--- {{cookiecutter.app_name}}/main.go
+++ {{cookiecutter.app_name}}/main.go
@@ -4,42 +4,14 @@ import (
 	"os"
 	"flag"
 	"context"
-	"math/rand"
-	"time"
 	"fmt"
 
 	"github.com/google/subcommands"
-	"{{cookiecutter.app_path}}/logging"
 	"{{cookiecutter.app_path}}/config"
 	"{{cookiecutter.app_path}}/http"
 )
 
 type initialHandler func() bool
-
-func initRandomSeed() bool {
-	rand.Seed(time.Now().Unix())
-	return true
-}
-
-func initConfiguration() bool {
-	var err error
-	err = config.Configuration.Read()
-	if err != nil {
-	    fmt.Fprintln(os.Stderr, err.Error())
-	}
-
-	err = config.Configuration.Validate()
-	if err != nil {
-	    fmt.Fprintln(os.Stderr, err.Error())
-	    return false
-	}
-	return true
-}
-
-func initLogging() bool {
-	logging.Init()
-	return true
-}
 
 func main() {
 	subcommands.Register(subcommands.HelpCommand(), "")
